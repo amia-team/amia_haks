2970105a95fb1655c9d9c4784e4324361ff375bc
Caelan L. Talmadge <34071000+cltalmadge@users.noreply.github.com>
2022-01-20 22:46:25 -0500

    Edited description, added warlock entry to class.2da

---
+++ windows-pack-all.bat
@@ -0,0 +1,33 @@
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose amia_top2_8"
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose amia_top_5"
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose amia_races_3"
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose amia_tile1_2"
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose amia_tile2_1"
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose amia_plc_3"
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose amia_items_1"
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose amia_rest_1"
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose amia_a_10"
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose amia_b_1"
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose amia_c_1"
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose amia_d_1"
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose amia_music_1"
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose amia_override_6"
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose amia_crea_6"
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose amia_skybox_1"
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose tlk"
