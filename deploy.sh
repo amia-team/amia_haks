275c7d5d6d1a10fb57f9879c99ac029bbfe887dc
Jes A Condrey <mydnite_rondera@hotmail.com>
2022-07-24 19:11:41 -0500

    Sudo update
    sudoooo

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# Check if run as sudo
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as sudo"
-  exit
-fi
 # set var to user provided directory
 dir=$1
 # check if directory exists
@@ -14,8 +9,8 @@ fi
 # Pack all content
 ./linux-pack-all.sh
 # copy *.hak to dir/hak
-cp ./*.hak $dir/hak
+sudo cp ./*.hak $dir/hak
 # copy *.tlk to dir/tlk
-cp ./*.tlk $dir/tlk
+sudo cp ./*.tlk $dir/tlk
 # Remove all tlk and hak files from current directory
-rm ./*.tlk ./*.hak
+sudo rm ./*.tlk ./*.hak
