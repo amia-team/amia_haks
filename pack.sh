f491715916e9f4ccc1d1fc23c58bbf91d85a2248
Caelan L. Talmadge <34071000+cltalmadge@users.noreply.github.com>
2024-02-24 21:21:49 -0600

    Delete unnecessary backup file, add permissions

--- pack.sh
+++ pack.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -t -v "$(pwd):/nasher" -v "$(pwd):/root/.local/share/Neverwinter Nights/$1/" nasher pack --clean --verbose --yes $2
+docker run --rm -t -u $(id -u):$(id -g) -v "$(pwd):/nasher" -v "$(pwd):/root/.local/share/Neverwinter Nights/$1/" nasher pack --clean --verbose --yes $2
