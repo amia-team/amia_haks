0fa2787cf0c561fa095f581b81234d4e9bbc2df4
Caelan L. Talmadge <talmadge12@gmail.com>
2025-03-06 09:15:50 -0600

    Create unpack.sh

---
+++ unpack.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run --rm -t -u $(id -u):$(id -g) -v "$(pwd):/nasher" -v "$(pwd):/root/.local/share/Neverwinter Nights/$1/" cltalmadge/nasher:1.1.1 unpack --clean --verbose --yes $2
