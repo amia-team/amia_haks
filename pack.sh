46ee2a39d65ca842db3ea2cbc4be623d71acaf3a
Caelan L. Talmadge <talmadge12@gmail.com>
2025-12-04 15:49:14 -0600

    Add Magyar style saber

--- pack.sh
+++ pack.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -t -u $(id -u):$(id -g) -v "$(pwd):/nasher" -v "$(pwd):/root/.local/share/Neverwinter Nights/$1/" cltalmadge/nasher:1.1.1 pack --clean --verbose --yes $2
+docker run --rm -t -u $(id -u):$(id -g) -v "$(pwd):/nasher" -v "$(pwd):/root/.local/share/Neverwinter Nights/$1/" cltalmadge/nasher:1.1.1 pack --clean --verbose --packUnchanged --yes $2
